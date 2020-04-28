bcaceec2dff898fd12877f86dd0d990b40205cb7
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2020-04-28 16:16:58 +0100

    Project created

---
+++ ProjectCourse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectCourse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
