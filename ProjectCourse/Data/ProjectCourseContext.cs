e51b2187a38ec81876add0255e1febab1e070290
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2020-04-30 15:52:00 +0100

    CRUD Scaffolding

---
+++ ProjectCourse/Data/ProjectCourseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectCourse.Models;
+
+namespace ProjectCourse.Data
+{
+    public class ProjectCourseContext : DbContext
+    {
+        public ProjectCourseContext (DbContextOptions<ProjectCourseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectCourse.Models.Department> Department { get; set; }
+    }
+}
--- ProjectCourse/Startup.cs
+++ ProjectCourse/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ProjectCourse.Data;
 
 namespace ProjectCourse
 {
@@ -33,6 +35,9 @@ namespace ProjectCourse
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjectCourseContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjectCourseContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
