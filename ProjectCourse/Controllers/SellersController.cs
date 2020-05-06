c8485e4b5aac279e6179de7a2d20b68613d074fe
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2020-05-06 17:17:22 +0100

    SellerFormViewModel and Department select component

--- ProjectCourse/Controllers/SellersController.cs
+++ ProjectCourse/Controllers/SellersController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using ProjectCourse.Models;
 using Microsoft.AspNetCore.Mvc;
 using ProjectCourse.Services;
+using ProjectCourse.Models.ViewModels;
 
 namespace ProjectCourse.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService seller)
+        public SellersController(SellerService seller, DepartmentService departmentService)
         {
             _sellerService = seller;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -23,7 +26,9 @@ namespace ProjectCourse.Controllers
         }
         public IActionResult Create()
         {
-            return View();
+            ICollection<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel) ;
         }
 
         [HttpPost]
---
+++ ProjectCourse/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using ProjectCourse.Data;
+using ProjectCourse.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectCourse.Services
+{
+    public class DepartmentService
+    {
+        private readonly ProjectCourseContext _context;
+
+        public DepartmentService(ProjectCourseContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
